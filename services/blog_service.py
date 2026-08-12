eaf8ec5d2748dada2bfc9240ecf0f9454e2c111d
Anisshaikh25 <anisshaikh5691@gmail.com>
2026-08-12 20:03:58 +0530

    created service for blog

---
+++ services/blog_service.py
@@ -0,0 +1,28 @@
+from database.repositories.blog_post_repository import BlogPostRepository
+
+
+class BlogService:
+
+    def __init__(self, db):
+        self.blog_repo = BlogPostRepository(db)
+
+    def process_blog(
+        self,
+        competitor_id: int,
+        blog_data: dict
+    ):
+        """
+        Process scraped blog data and save it
+        using BlogPostRepository.
+        """
+
+        blog = self.blog_repo.get_or_create(
+            competitor_id=competitor_id,
+            title=blog_data["title"],
+            url=blog_data["url"],
+            author=blog_data.get("author"),
+            content=blog_data["content"],
+            published_at=blog_data.get("published_at"),
+        )
+
+        return blog
