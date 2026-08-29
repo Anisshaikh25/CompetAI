44496ff1d45a5f82d9debb30cc8727e07a628662
Anisshaikh25 <anisshaikh5691@gmail.com>
2026-08-29 23:33:40 +0530

    created a script to fetch a bookdio website through a request-based scrapper

---
+++ tests/test_bookdio_connection.py
@@ -0,0 +1,48 @@
+# from scrapers.base_scraper import BaseScraper
+
+
+# scraper = BaseScraper()
+
+# try:
+
+#     url = "https://www.bookdio.org/all-books"
+
+#     html = scraper.fetch_page(url)
+
+#     print("Successfully fetched Bookdio!")
+
+#     print("\nHTML Length:")
+#     print(len(html))
+
+#     print("\nFirst 1000 characters:\n")
+#     print(html[:1000])
+
+# finally:
+
+#     scraper.close()
+
+#     ##output
+#      python -m tests.test_bookdio_connection
+# Successfully fetched Bookdio!
+
+# HTML Length:
+# 1692242
+
+# First 1000 characters:
+
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+  
+#   <meta charset='utf-8'>
+#   <meta name="viewport" content="width=device-width, initial-scale=1" id="wixDesktopViewport" />
+#   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+#   <meta name="generator" content="Wix.com Website Builder"/>
+
+#   <link rel="icon" sizes="192x192" href="https://static.wixstatic.com/media/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png" type="image/png"/>
+#   <link rel="shortcut icon" href="https://static.wixstatic.com/media/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png" type="image/png"/>
+#   <link rel="apple-touch-icon" href="https://static.wixstatic.com/media/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png/v1/fill/w_180%2Ch_180%2Clg_1%2Cusm_0.66_1.00_0.01/ef8af1_06866286c2314317b1353beae89ac942%7Emv2.png" type="image
+
+
+
+
